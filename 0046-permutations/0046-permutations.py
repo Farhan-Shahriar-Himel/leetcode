842bcfc47967c2e15b370317c2fdc1e1835dd8e8
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2024-12-14 23:03:30 +0600

    Time: 3 ms (36.31%), Space: 17.5 MB (8.81%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,4 +2,20 @@ from itertools import permutations
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
+        n = len(nums)
+        res, tmp, visit = [], [], set()
+        
+        def backtrack():
+            if len(tmp) == n:
+                res.append(tmp[:])
+            else:
+                for i in range(n):
+                    if i not in visit:
+                        visit.add(i)
+                        tmp.append(nums[i])
+                        backtrack()
+                        visit.remove(i)
+                        tmp.pop()
+        backtrack()
+        
+        return res
