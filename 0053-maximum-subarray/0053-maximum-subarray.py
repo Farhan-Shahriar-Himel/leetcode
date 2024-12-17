f4d3190d3c91aaf9d53479a5a55c12bbc4fe95a5
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2024-12-17 22:07:17 +0600

    Time: 86 ms (34.02%), Space: 32.9 MB (7.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        tot = 0
-        ans = -(1e9 + 7)
-        for el in nums:
-            tot += el
-            ans = max(tot, ans)
-            if tot < 0:
-                tot = 0
-        return ans
+        res = tot = -float('inf')
+        for num in nums:
+            tot = max(num, tot + num)
+            res = max(res, tot)
+        return res
