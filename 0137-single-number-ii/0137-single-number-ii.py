94680a1bdc8f46e88d0eaaa7470260c89bfeae88
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2024-12-25 11:50:13 +0600

    Time: 3 ms (68.83%), Space: 19.1 MB (19.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,12 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(32):
-            setBit = 0
-            for num in nums:
-                if num & (1 << i) != 0:
-                    setBit += 1
-            if setBit % 3 == 1:
-                ans = ans | (1 << i)
-        
-        return ans - (1 << 32) if ans >= (1 << 31) else ans
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & (~twos)
+            twos = (twos ^ num) & (~ones)
+        return ones
