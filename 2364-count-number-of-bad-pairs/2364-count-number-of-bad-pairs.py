8362b32ec04ca7d66c32b8dadb49d59859b9cba5
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2025-02-09 07:21:08 +0600

    Time: 63 ms (89.76%), Space: 38.9 MB (28.81%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -1,9 +1,13 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         n, good = len(nums), 0
-        diff = defaultdict(int)
+        diff = dict()
         for i, num in enumerate(nums):
-            good += diff[i - num]
-            diff[i - num] += 1
+            d = i - num
+            if d in diff:
+                good += diff[d]
+                diff[d] += 1
+            else: 
+                diff[d] = 1
         
         return ((n * (n - 1)) // 2) - good
