179871c6aeeb7e81f96bd870eb39519d9cdef920
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2025-05-08 20:08:19 +0600

    Time: 2152 ms (49.35%), Space: 90 MB (82.83%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
@@ -17,7 +17,7 @@ class Solution:
             time_map[0][0] = 0
 
             while heap:
-                i, j, t, b = heapq.heappop(heap)
+                t, i, j, b = heapq.heappop(heap)
 
                 if t > time_map[i][j]:
                     continue
@@ -36,7 +36,7 @@ class Solution:
 
                     if wt < time_map[x][y]:
                         time_map[x][y] = wt
-                        heapq.heappush(heap, [x, y, time_map[x][y], 1 if b == 2 else 2])
+                        heapq.heappush(heap, [time_map[x][y], x, y, 1 if b == 2 else 2])
             
             return 0
         
