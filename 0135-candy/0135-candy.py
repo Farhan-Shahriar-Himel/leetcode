7913b5189463f02ba41c833f2ca4534625272d14
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2025-06-02 19:53:49 +0600

    Time: 11 ms (89.23%), Space: 18.9 MB (100%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -7,7 +7,7 @@ class Solution:
                 candies[i] = candies[i - 1] + 1
         
         for i in range(n - 2, -1, -1):
-            if ratings[i] > ratings[i + 1]:
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
                 candies[i] = candies[i + 1] + 1
         
         return sum(candies)
